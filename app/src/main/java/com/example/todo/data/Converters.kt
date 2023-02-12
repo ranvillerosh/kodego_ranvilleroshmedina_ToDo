cd50f81558ecb9269b579e5bcb9210ca409bc946
ranvillerosh <76862237+ranvillerosh@users.noreply.github.com>
2023-02-13 01:20:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/todo/data/Converters.kt
@@ -0,0 +1,36 @@
+package com.example.todo.data
+
+import androidx.room.TypeConverter
+import java.sql.Date
+import java.time.LocalDate
+import java.time.LocalTime
+
+class Converters {
+    @TypeConverter
+    fun dateToString(date:LocalDate?):String? {
+        return date?.toString()
+    }
+
+    @TypeConverter
+    fun stringToDate(date:String?):LocalDate? {
+        return if(date == null) {
+            null
+        } else {
+            LocalDate.parse(date)
+        }
+    }
+
+    @TypeConverter
+    fun timeToString(time: LocalTime?):String? {
+        return time?.toString()
+    }
+
+    @TypeConverter
+    fun stringToTime(time:String?):LocalTime? {
+        return if(time == null) {
+            null
+        } else {
+            LocalTime.parse(time)
+        }
+    }
+}
